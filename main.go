4c6ad3dc24712c9502f1a4bad45dd50a2ac76bba
jiayuebao <jiayueb@andrew.cmu.edu>
2019-02-17 18:04:21 -0500

    post and search

--- main.go
+++ main.go
@@ -1,78 +1,197 @@
 package main
 
 import (
-        "encoding/json"
-        "fmt"
-        "net/http"
-        "log"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"reflect"
+	"strconv"
+
+	"github.com/olivere/elastic"
+	"github.com/pborman/uuid"
 )
 
 type Location struct {
-    Lat float64 `json:"lat"`
-    Lon float64 `json:"lon"`
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
 }
 
 type Post struct {
-    User    string  `json:"user"`
-    Message string  `json:"message"`
-    Location Location `json:"location"`
+	User     string   //`json:"user"`
+	Message  string   `json:"message"`
+	Location Location `json:"location"`
 }
 
-const {
-    DISTANCE = "200km"
-}
+const (
+	POST_INDEX = "post"
+	POST_TYPE  = "post"
+
+	DISTANCE = "200km"
+
+	// Needs to update this URL if you deploy it to cloud.
+	ES_URL = "http://35.222.222.92:9200"
+)
 
 func main() {
 	fmt.Println("started-service")
-    http.HandleFunc("/post", handlerPost)
-    http.HandleFunc("/search", handlerSearch)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	createIndexIfNotExist()
+
+	http.HandleFunc("/post", handlerPost)
+	http.HandleFunc("/search", handlerSearch)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func createIndexIfNotExist() {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+
+	exists, err := client.IndexExists(POST_INDEX).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	if !exists {
+		mapping := `{
+            "mappings": {
+                "post": {
+                    "properties": {
+                        "location": {
+                            "type": "geo_point"
+                        }
+                    }
+                }
+            }
+        }`
+		_, err = client.CreateIndex(POST_INDEX).Body(mapping).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}
+
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
-    // Parse from body of request to get a json object.
-    fmt.Println("Received on post request")
-    decoder := json.NewDecoder(r.Body)
-    var p Post
-    // standard form in go to check error
-    if err := decoder.Decode(&p); err != nil {
-            panic(err)
-    }
+	// Parse from body of request to get a json object.
+	fmt.Println("Received on post request")
 
-    fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	decoder := json.NewDecoder(r.Body)
+	var p Post
+	if err := decoder.Decode(&p); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "Post received: %v %s %v %v\n", p.User, p.Message,
+		p.Location.Lat, p.Location.Lon)
+
+	id := uuid.New()
+	err := saveToES(&p, id)
+	if err != nil {
+		http.Error(w, "Failed to save post to ElasticSearch",
+			http.StatusInternalServerError)
+		fmt.Printf("Failed to save post to ElasticSearch %v.\n", err)
+		return
+	}
+	fmt.Printf("Saved one post to ElasticSearch: %s", p.Message)
+
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Received on post request for search")
-    lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"),64)
-    lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"),64)
-    // range is optional
-    ran := DISTANCE
-    if val := r.URL.Query().Get("range"); val !="" {
-        ran = val + "km"
-    }
+	fmt.Println("Received one request for search")
 
-    fmt.Println("range is", ran)
-    // Return a fake post
-      p := &Post{
-             User:"1111",
-             Message:"message",
-             Location: Location{
-                    Lat:lat,
-                    Lon:lon,
-             },
-      }
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 
-      js, err := json.Marshal(p)
-      if err != nil {
-             panic(err)
-             return
-      }
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+	// range is optional
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
 
-      w.Header().Set("Content-Type", "application/json")
-      w.Write(js)
+	fmt.Println("range is ", ran)
 
+	posts, err := readFromES(lat, lon, ran)
+	if err != nil {
+		http.Error(w, "Failed to read post from ElasticSearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to read post from ElasticSearch %v.\n", err)
+		return
+	}
 
+	js, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
+		return
+	}
+	w.Write(js)
 
-    fmt.Fprintf(w, "Search received: %s %s\n", lat, lon)
+}
+
+// Save a post to ElasticSearch
+func saveToES(post *Post, id string) error {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return err
+	}
+
+	_, err = client.Index().
+		Index(POST_INDEX).
+		Type(POST_TYPE).
+		Id(id).
+		BodyJson(post).
+		Refresh("wait_for").
+		Do(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Post is saved to index: %s\n", post.Message)
+	return nil
+}
+
+func readFromES(lat, lon float64, ran string) ([]Post, error) {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return nil, err
+	}
+
+	query := elastic.NewGeoDistanceQuery("location")
+	query = query.Distance(ran).Lat(lat).Lon(lon)
+
+	searchResult, err := client.Search().
+		Index(POST_INDEX).
+		Query(query).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	// searchResult is of type SearchResult and returns hits, suggestions,
+	// and all kinds of other information from Elasticsearch.
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization. If you want full control
+	// over iterating the hits, see below.
+	var ptyp Post
+	var posts []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(ptyp)) { // Object a = test;
+		if p, ok := item.(Post); ok { // Post p = (Post) a;
+			posts = append(posts, p)
+		}
+	}
+
+	return posts, nil
 }
