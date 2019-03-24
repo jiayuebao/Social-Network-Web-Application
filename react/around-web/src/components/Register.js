5ab214feeac5e44a8b0f2ce524063b0bebcc6f87
jiayuebao <jiayueb@andrew.cmu.edu>
2019-03-24 01:28:52 -0400

    finished signup

---
+++ react/around-web/src/components/Register.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import {Form, Input, Button, message} from 'antd';
+import { API_ROOT } from '../constants';
+
+const FormItem = Form.iterm;
+
+class RegistrationForm extends React.Component {
+    state = {
+      confirmDirty: false,
+      autoCompleteResult: [],
+    };
+  
+    handleSubmit = (e) => {
+      e.preventDefault();
+      this.props.form.validateFieldsAndScroll((err, values) => {
+        if (!err) {
+            fetch(`${API_ROOT}/signup`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    username: values.username,
+                    password: values.password,
+                }),
+            }).then((response) => {
+                if (response.ok) {
+                    return response;
+                }
+                throw new Error(response.statusText);
+            }).then(() => {
+                message.success('Registration Succeed');
+            }).catch((e) => {
+                message.error('Registration Failed');
+                console.log(e);
+            })
+        }
+      });
+    }
+  
+    handleConfirmBlur = (e) => {
+      const value = e.target.value;
+      this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    }
+  
+    compareToFirstPassword = (rule, value, callback) => {
+      const form = this.props.form;
+      if (value && value !== form.getFieldValue('password')) {
+        callback('Two passwords that you enter is inconsistent!');
+      } else {
+        callback();
+      }
+    }
+  
+    validateToNextPassword = (rule, value, callback) => {
+      const form = this.props.form;
+      if (value && this.state.confirmDirty) {
+        form.validateFields(['confirm'], { force: true });
+      }
+      callback();
+    }
+  
+    render() {
+      const { getFieldDecorator } = this.props.form;
+  
+      const formItemLayout = {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 8 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+      };
+      const tailFormItemLayout = {
+        wrapperCol: {
+          xs: {
+            span: 24,
+            offset: 0,
+          },
+          sm: {
+            span: 16,
+            offset: 8,
+          },
+        },
+      };
+      
+      return (
+        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+          <Form.Item
+            label="username"
+          >
+            {getFieldDecorator('username', {
+              rules: [{
+                required: true, message: 'Please input your username!', whitespace: false
+              }],
+            })(
+              <Input />
+            )}
+          </Form.Item>
+          <Form.Item
+            label="Password"
+          >
+            {getFieldDecorator('password', {
+              rules: [{
+                required: true, message: 'Please input your password!',
+              }, {
+                validator: this.validateToNextPassword,
+              }],
+            })(
+              <Input type="password" />
+            )}
+          </Form.Item>
+          <Form.Item
+            label="Confirm Password"
+          >
+            {getFieldDecorator('confirm', {
+              rules: [{
+                required: true, message: 'Please confirm your password!',
+              }, {
+                validator: this.compareToFirstPassword,
+              }],
+            })(
+              <Input type="password" onBlur={this.handleConfirmBlur} />
+            )}
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">Register</Button>
+          </Form.Item>
+        </Form>
+      );
+    }
+  }
+  
+export const Register = Form.create()(RegistrationForm);
